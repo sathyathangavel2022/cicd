ce0d730b571f01257ef7e4fbcf8c2044e9548ac9
sathyathangavel2022 <sathyathangavel2022@gmail.com>
2026-02-25 10:47:10 +0000

    modified

--- variables.tf
+++ variables.tf
@@ -2,11 +2,12 @@ variable "project_id" {
   description = "The GCP project ID"
   type        = string
 }
-
+/*
 variable "region" {
   description = "The GCP region"
   type        = string
 }
+*/
 variable "service_accounts" {
   description = "Map of service accounts to create"
   type = map(object({
